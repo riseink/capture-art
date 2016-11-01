c51ed3f3fddec795b5803257d2f715c513f52d02
Digital Traffic <digitaltraffic@digitraf2.local>
2016-11-01 17:04:44 -0500

    safari 10 bug fix

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -29,7 +29,10 @@ var app = {
 		    // Firefox 1.0+
 		var isFirefox = typeof InstallTrigger !== 'undefined';
 		    // At least Safari 3+: "[object HTMLElementConstructor]"
-		var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+		// var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+		var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 || !isChrome && !isOpera && window.webkitAudioContext !== undefined;
+		
+		
 		    // Internet Explorer 6-11
 		var isIE = /*@cc_on!@*/false || !!document.documentMode;
 		    // Edge 20+
